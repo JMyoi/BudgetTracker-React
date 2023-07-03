4810e7b9bd22ea8e892e2156b2043c0425c6d3ba
Jay Chen <jaychenjjc@gmail.com>
2023-07-03 11:36:24 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -44,8 +44,8 @@ const[Open, setOpen] = useState(false);
           date: e.target.dateInput.value,
           description: e.target.descriptionInput.value,
           category: e.target.categoryInput.value,
-          amount: e.target.amountInput.value
-
+          amount: e.target.amountInput.value,
+          note:""
         }, ...transactions]
         //form.reset(); if you want the form to reset.
       );
@@ -69,41 +69,20 @@ const[Open, setOpen] = useState(false);
           <input type="submit" value = "Save"/>
           </div>
         </form>
-        {/* TableContent transactionInputs = {transactionInputs} setTransactionInputs={setTransactionInputs}/> */}
         </>
       );
     }else{
       return(
         <>
           <button type = "button" onClick = {()=>setOpen(true)} className = 'AddTransactionBtn'>Add Transaction</button>
-          {/* <TableContent transactionInputs = {transactionInputs} setTransactionInputs={setTransactionInputs}/> */}
         </>
       );
     }
 }
 
 
-
-
-function TableContent({transactions, setTransactions }){
-
-// function handleRemove({inputs}){
-//   setTransactions(
-//     transactions.filter(
-//         transaction=>{
-//          return(transaction.date!==inputs.date &&
-//           transaction.description!== inputs.description &&
-//           transaction.category!==inputs.category &&
-//           transaction.amount!==inputs.amount)
-//         })
-//   );
-// }
-
-
 //this removes transactions that are the exact same.
-//try transaction!==Inputs
-function RemoveButton({Inputs}){
-  
+function RemoveButton({Inputs, transactions, setTransactions}){
   return(
    <td className = "editBtns">
     <button type = "button" onClick = {()=>(setTransactions(transactions.filter(
@@ -114,22 +93,30 @@ function RemoveButton({Inputs}){
             transaction.category!==Inputs.category ||
             transaction.amount!==Inputs.amount 
           )
-    } )))}>Remove</button>
+    } )))}>Remove This and all duplicates</button>
       <button type = "button" >Edit</button>
       </td>
   );
 } 
 
+function Note({transactions, setTransactions}){
+  const[show, setShow] = useState(false);
+  const[note, setNote] = useState('');
 
-// function RemoveByKeyButton({key}){
-//   return(
-//     <td className = "editBtns">
-//       <button type = "button" onClick = {()=>(
-//          setTransactions(transactions.filter((index) => index !== key)))} >Remove This</button>
-//     </td> 
-//   );
-// }
+  function addNote(){
+    setNote()
+  }
 
+  return (
+    <>
+    <button onClick = {()=>setShow(true)} ><img className = "NoteButton" src="src/images/speechbubble.png" alt="speech bubble" /></button>
+    {show?  <textarea value = {transactions.note} onChange={addNote}/>: <></>}
+    </>
+  );
+}
+
+
+function TableContent({transactions, setTransactions }){
 
   return (
     <>
@@ -151,18 +138,21 @@ function RemoveButton({Inputs}){
               return(
                 <>
                 <tr className = "inputRow" key = {i}>
-                  <RemoveButton Inputs = {Inputs}/>
+                  
+                  {/*this button removes only that transaction by comparing index*/}
                   <td className = "editBtns">
                       <button type = "button" onClick = {()=>(
                         setTransactions(transactions.filter((transaction, index) => index !== i)))} >Remove
                      </button>
                   </td> 
+                  <RemoveButton Inputs = {Inputs} transactions = {transactions} setTransactions = {setTransactions}/>
 
                   <td className = "tableData">{Inputs.date} </td>
                   <td className = "tableData">{Inputs.description} </td>
                   <td className = "tableData">{Inputs.category}</td>
                   <td className = "tableData">{Inputs.amount}</td>
                   <td className="tableData">Key:{i}</td> {/* Display the key */}
+                  <Note transactions={transactions} setTransactions = {setTransactions} />
                 </tr>
                 </>
                 )
@@ -176,15 +166,7 @@ function RemoveButton({Inputs}){
     </>
   )
 }
-/*          
-      <td className = "editBtns">
-                  <button type = "button" onClick = {()=>(
-                     setTransactions(
-                      transactions.filter((transaction, index) => index !== i)))} >Remove</button>
-                  <button type = "button" >Edit</button>
-                  </td> 
-                  
-*/
+
 
 
 
@@ -198,74 +180,3 @@ export default function App() {
 }
 
 
-
-// function BudgetTracker(){
-//   //this state holds an array of objects with transaction, data, and amount keys.
-//   const[transactionInputs, setTransactionInputs] = useState([]);  
-  
-//   function Form(){
-//     function handleSubmit(e){
-//       e.preventDefault();
-//         setTransactionInputs(
-//           [{
-//             transaction: e.target.transactionInput.value,
-//             date: e.target.dateInput.value,
-//             amount: e.target.amountInput.value
-//           },
-//           ...transactionInputs]
-//         );
-  
-//       } 
-      
-//     return(
-//       <form onSubmit = {handleSubmit} class = "Form">
-//       <label htmlFor = "transactionInput"> Transaction: </label>
-//         <input id = "transactionInput" type="text" required placeholder='Name of Transaction'/>
-//       <label htmlFor ="dateInput" >date: </label>
-//         <input id = "dateInput" type="date" required/>
-//       <label htmlFor = "amountInput">amount: </label>
-//         <input id ="amountInput" type="number" required placeholder = '- for spendings and + for earnings'/>
-//       <input type="submit" />
-//     </form>
-//     )
-  
-//   }
-  
-//     return (
-//       <>
-//       <Form/>
-//       <div id = "ContentBody">
-//         <table id = "TableContent">
-//           <thead>
-//             <tr id = "headerRow">
-//               <th>Transaction</th>
-//               <th>Date</th>
-//               <th>Amount</th>
-//             </tr>
-//           </thead>
-  
-//           <tbody id = "dataBody">
-//             {transactionInputs.map(
-//               (Inputs)=>{
-//                 let transaction = Inputs.transaction;
-//                 let date = Inputs.date;
-//                 let amount = Inputs.amount;
-//                 return(
-//                   <tr className = "inputRow">
-//                     <td className = "tableData">{transaction} </td>
-//                     <td className = "tableData">{date} </td>
-//                     <td className = "tableData">{amount}</td>
-//                   </tr>
-//                   )
-//                 }
-//               )
-//             }
-//           </tbody>
-//         </table>
-  
-//       </div>
-//       </>
-//     )
-  
-  
-//   }
