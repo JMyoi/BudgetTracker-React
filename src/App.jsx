f93645e12af34f15b5589636a947b5248bdf133f
Jay Chen <jaychenjjc@gmal.com>
2023-07-04 17:01:02 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function BudgetTracker() {
@@ -80,7 +78,11 @@ function BudgetTracker() {
 }
 
 function App() {
-  return <BudgetTracker/>;
+  return (
+    <>
+  <BudgetTracker/>
+    </>
+  );
 }
 
 export default App;
